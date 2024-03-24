a036ed04e6b831ba58b6b79e7d9b6c9f3714bcc8
Raphael Rodrigues <73262792+raphadvlp@users.noreply.github.com>
2024-03-23 23:32:18 -0300

    Abrindo restaurante

--- script.js
+++ script.js
@@ -261,7 +261,8 @@ checkoutBtn.addEventListener("click", function() {
 function checkRestauranteOpen() {
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 23; //Vai devolver como True (Restaurante está aberto)
+    return hora >= 18 
+    //&& hora < 23; //Vai devolver como True (Restaurante está aberto)
 }
 
 const spanItem = document.getElementById("date-span");
